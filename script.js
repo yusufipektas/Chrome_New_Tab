85a35dd1582acedd6f047e11daa61e86aaee5899
ali-hamas <alihamasghurki4@gmail.com>
2023-12-29 19:08:36 +0500

    update

--- script.js
+++ script.js
@@ -25,76 +25,77 @@ const dayNames = [
   "Sunday",
 ];
 
-// Getting Date
-let a = new Date(),
-  bHour = a.getHours(),
-  hour = 0,
-  bMin = a.getMinutes(),
-  min = 0,
-  bDay = a.getDay(),
-  day = dayNames[bDay - 1],
-  date = a.getDate(),
-  bMonth = a.getMonth(),
-  month = monthNames[bMonth - 1],
-  year = a.getFullYear(),
-  // Selecting Clock ID's
-  hourID = document.getElementById("hour"),
-  minID = document.getElementById("min"),
-  zoneID = document.getElementById("zone"),
-  dayID = document.getElementById("day"),
-  dateID = document.getElementById("date"),
-  monthID = document.getElementById("month"),
-  yearID = document.getElementById("year"),
-  dayTimeID = document.getElementById("dayTime");
-
-//Hour format
-function hourFormat() {
-  if (bHour === 0) {
-    hour = 12;
-  } else if (bHour > 12) {
-    hour = bHour - 12;
-  } else {
-    hour = bHour;
-  }
-}
-
-// Time Zone
-function timeZone() {
-  if (bHour > 11) {
-    zoneID.innerHTML = "PM";
-  } else {
-    zoneID.innerHTML = "AM";
-  }
-}
-
-// Day Time
-function dayTime() {
-  if (bHour >= 5 && bHour < 12) {
-    dayTimeID.innerHTML = "Morning";
-  } else if (bHour >= 12 && bHour < 16) {
-    dayTimeID.innerHTML = "After Noon";
-  } else if (bHour >= 16 && bHour < 21) {
-    dayTimeID.innerHTML = "Evening";
-  } else {
-    dayTimeID.innerHTML = "Night";
-  }
-}
-
-// Adding 0 to minutes
-function min0() {
-  if (bMin < 10) {
-    min = `0${bMin}`;
-  } else {
-    min = bMin;
-  }
-}
-
 setInterval(() => {
+  // Getting Date
+  let a = new Date(),
+    bHour = a.getHours(),
+    hour = 0,
+    bMin = a.getMinutes(),
+    min = 0,
+    bDay = a.getDay(),
+    day = dayNames[bDay - 1],
+    date = a.getDate(),
+    bMonth = a.getMonth(),
+    month = monthNames[bMonth - 1],
+    year = a.getFullYear(),
+    // Selecting Clock ID's
+    hourID = document.getElementById("hour"),
+    minID = document.getElementById("min"),
+    zoneID = document.getElementById("zone"),
+    dayID = document.getElementById("day"),
+    dateID = document.getElementById("date"),
+    monthID = document.getElementById("month"),
+    yearID = document.getElementById("year"),
+    dayTimeID = document.getElementById("dayTime");
+
+  //Hour format
+  function hourFormat() {
+    if (bHour === 0) {
+      hour = 12;
+    } else if (bHour > 12) {
+      hour = bHour - 12;
+    } else {
+      hour = bHour;
+    }
+  }
+
+  // Time Zone
+  function timeZone() {
+    if (bHour > 11) {
+      zoneID.innerHTML = "PM";
+    } else {
+      zoneID.innerHTML = "AM";
+    }
+  }
+
+  // Day Time
+  function dayTime() {
+    if (bHour >= 5 && bHour < 12) {
+      dayTimeID.innerHTML = "Morning";
+    } else if (bHour >= 12 && bHour < 16) {
+      dayTimeID.innerHTML = "After Noon";
+    } else if (bHour >= 16 && bHour < 21) {
+      dayTimeID.innerHTML = "Evening";
+    } else {
+      dayTimeID.innerHTML = "Night";
+    }
+  }
+
+  // Adding 0 to minutes
+  function min0() {
+    if (bMin < 10) {
+      min = `0${bMin}`;
+    } else {
+      min = bMin;
+    }
+  }
+
   timeZone();
   hourFormat();
   dayTime();
   min0();
 
+  // Printing Time
   hourID.innerHTML = hour;
   minID.innerHTML = min;
   dayID.innerHTML = day;
@@ -112,7 +113,7 @@ function searchGoogle(event) {
   window.location.href = googleSearchUrl;
 }
 
-// <--------------------------------Setting-------------------------------->
+// <--------------------------------  Setting  -------------------------------->
 
 // Selecting ID's
 let container = document.getElementById("container"),
@@ -122,9 +123,7 @@ let container = document.getElementById("container"),
 // Show Setting
 function openSetting() {
   settingBox.style.width = "350px";
-  setTimeout(() => {
-    openSettingBtn.style.display = "none";
-  }, 100);
+  openSettingBtn.style.display = "none";
 }
 // Close Setting
 function closeSetting() {
@@ -141,6 +140,8 @@ window.onclick = function (event) {
     closeSetting();
   }
 };
+
+// <--------------------------------  Wallpaper  Setting  -------------------------------->
 
 // Container Dim
 function containerDim() {
@@ -163,49 +164,79 @@ function containerBlur() {
   localStorage.setItem("blur", blurRange);
 }
 
-// Show Hide Time Container
+// <--------------------------------  Time  Setting  -------------------------------->
+
+// Time Container Visibility
 let timeCheckbox = document.getElementById("time-checkbox"),
   timeContainer = document.getElementById("time-container");
-function timeContainerDisplay() {
-  const timeContainerHeight = timeCheckbox.checked ? "125px" : "0";
-  const timeContainerPaddingTop = timeCheckbox.checked ? "20px" : "0";
+function timeContVisibility() {
+  const timeContHeight = timeCheckbox.checked ? "130px" : "0";
+  const timeContPaddingTop = timeCheckbox.checked ? "20px" : "0";
+  const timeContMarginBottom = timeCheckbox.checked ? "15px" : "0";
 
-  timeContainer.style.height = timeContainerHeight;
-  timeContainer.style.paddingTop = timeContainerPaddingTop;
+  timeContainer.style.height = timeContHeight;
+  timeContainer.style.paddingTop = timeContPaddingTop;
+  timeContainer.style.marginBottom = timeContMarginBottom;
 
   localStorage.setItem(
-    "timeContainerDisplay",
-    JSON.stringify({ timeContainerHeight, timeContainerPaddingTop })
+    "timeContVisibility",
+    JSON.stringify({
+      timeContHeight,
+      timeContPaddingTop,
+      timeContMarginBottom,
+      timeChecked: timeCheckbox.checked,
+    })
   );
 }
-timeCheckbox.addEventListener("change", timeContainerDisplay);
+timeCheckbox.addEventListener("change", timeContVisibility);
 
-// Show Hide Search Container
+// <--------------------------------  Message  Setting  -------------------------------->
+
+// Message Container Visibility
+let msgCheckbox = document.getElementById("msg-checkbox"),
+  msgContainer = document.getElementById("msg-container");
+function msgContVisibility() {
+  const msgContHeight = msgCheckbox.checked ? "60px" : "0";
+  const msgContMarginBottom = msgCheckbox.checked ? "15px" : "0";
+
+  msgContainer.style.height = msgContHeight;
+  msgContainer.style.marginBottom = msgContMarginBottom;
+
+  localStorage.setItem(
+    "msgContVisibility",
+    JSON.stringify({
+      msgContHeight,
+      msgContMarginBottom,
+      msgChecked: msgCheckbox.checked,
+    })
+  );
+}
+msgCheckbox.addEventListener("change", msgContVisibility);
+
+// <--------------------------------  Search  Setting  -------------------------------->
+
+// Search Container Visibility
 let searchCheckbox = document.getElementById("search-checkbox"),
   searchContainer = document.getElementById("search-container");
-function searchContainerDisplay() {
-  if (searchCheckbox.checked === true) {
-    searchContainer.style.display = "block";
-    setTimeout(() => {
-      searchContainer.style.height = "50px";
-    }, 200);
-  } else {
-    searchContainer.style.height = "0";
-    setTimeout(() => {
-      searchContainer.style.display = "none";
-    }, 400);
-  }
-  // const searchContainerHeight = searchCheckbox.checked ? "50px" : "0";
-  // let searchContainerHiden = searchCheckbox.checked ? "block" : "none";
+function searchContVisibility() {
+  const searchContHeight = searchCheckbox.checked ? "50px" : "0";
+  const searchContMarginBottom = searchCheckbox.checked ? "15px" : "0";
 
-  // searchContainer.style.height = searchContainerHeight;
-  // setTimeout(() => {
-  //   searchContainer.style.display = searchContainerHiden;
-  // }, 400);
+  searchContainer.style.height = searchContHeight;
+  searchContainer.style.marginBottom = searchContMarginBottom;
 
-  // localStorage.setItem("searchContainerHeight", searchContainerHeight);
+  localStorage.setItem(
+    "searchContVisibility",
+    JSON.stringify({
+      searchContHeight,
+      searchContMarginBottom,
+      searchChecked: searchCheckbox.checked,
+    })
+  );
 }
-searchCheckbox.addEventListener("change", searchContainerDisplay);
+searchCheckbox.addEventListener("change", searchContVisibility);
+
+// <--------------------------------  Shortcuts  Setting  -------------------------------->
 
 // Border Radius
 function hello(element) {
